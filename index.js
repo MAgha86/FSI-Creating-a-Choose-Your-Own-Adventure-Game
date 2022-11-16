c934aba734ebcfddaeedcf91680b934e7b19bfb0
unknown <aghayev.emin@gmail.com>
2022-11-15 19:34:06 -0800

    game

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 //Your Code here
+let firstAnswer = window.prompt("Do you head left or right?")
+console.log("The value of variable firstAnswer is ", firstAanswer)
+
+//check the answer
+if(firstAnswer==="left") {
+    let secondAnswer = window.prompt("You encounter a dragon! Do you fight or flee?")
+    console.log("The value of veriable secondAnswer is ", secondAnswer)
+
+     if (secondAnswer==="fight") {
+        console.log("You won!")
+     } else if (secondAnswer==="flee"){
+        console.log("You lost!")
+     } else {
+        console.log("Game over. Try again!")
+     }
+     
+} else if(firstAnswer==="right") {
+    console.log("Continue!")
+} else {
+    console.log("Game over. Try again!")
+}
